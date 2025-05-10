80f30024fa5c9ff3606973145ff87180c35b5da1
pingweiii <pwchen31@gmail.com>
2025-05-10 20:25:51 +0800

    feat (index.php) added the base layout of the landing page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class = "box">
+    <a href = "equipment.php?muscle = chest">Chest</a>
+    <a href = "equipment.php?muscle = Shoulders">Shoulder</a>
+    <a href = "equipment.php?muscle = Arms">Arm</a>
+    <a href = "equipment.php?muscle = Back">Back</a>
+    <a href = "equipment.php?muscle = Legs">Legs</a>
+</div>
+</body>
+</html>
