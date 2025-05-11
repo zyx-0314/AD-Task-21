3e597ae5d6c2b1c1f449c57a445635141a9eaac3
pingweiii <pwchen31@gmail.com>
2025-05-11 22:03:23 +0800

    refactor (index.php) renamed the title of the page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Workout Planner - Select Muscle Group</title>
+    <title>Workout Plan</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
