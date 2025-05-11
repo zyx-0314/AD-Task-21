e7c63b0234484170aaa0ebe1924ea4b23308d3df
pingweiii <pwchen31@gmail.com>
2025-05-12 00:33:04 +0800

    fix (index.php) change the form to button

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="container">
         <h1>Workout Planner</h1>
         <h2>Select Your Muscle Group</h2>
-        
+
         <form action="page/workout.php" method="post">
             <div class="muscle-group">
                 <?php
@@ -22,7 +22,7 @@
                     "Shoulders" => "shoulders",
                     "Core" => "core"
                 );
-                
+
                 foreach ($muscleGroups as $name => $value) {
                     echo '<button type="submit" name="muscle_group" value="' . $value . '" class="muscle-option">' . $name . '</button>';
                 }
